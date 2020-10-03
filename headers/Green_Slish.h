85f2ff1eda9b1c8af61dfb3c3bef7b95e2162b13
Emma Alderson <emmaalderson24@gmail.com>
2020-10-03 16:53:51 -0400

    Set up eclipse for linux
    Remain using code::blocks for windows builds.

--- headers/Green_Slish.h
+++ headers/Green_Slish.h
@@ -1,8 +1,15 @@
 #pragma once
 
 #include "Entity.h"
+#include <SDL2/SDL.h>
+#include <vector>
 
 class Green_Slish: Entity
 {
-
+private:
+protected:
+    virtual void tick(std::vector<SDL_Event>, int);
+public:
+    virtual void render();
+    virtual void spawn();
 };
